60753b9db324389f08361280be64fc0d314ecbee
Koby Grah <78676376+KJG24@users.noreply.github.com>
2024-11-14 16:01:51 -0800

    altered small logic

--- makeTheFileSmallerButNotTooSMall.py
+++ makeTheFileSmallerButNotTooSMall.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 if __name__ == "__main__":
-    lineSize = 500
+    lineSize = 2500
     fileName = "Data/train.csv"
     newFile = "small.csv"
     data = pd.read_csv(fileName)
-    copiedRows = data.iloc[0:lineSize]
-    copiedRows.to_csv(newFile, index=False)
+    copiedRows = data.iloc[350 : lineSize + 350]
+    copiedRows.to_csv(newFile)
     
